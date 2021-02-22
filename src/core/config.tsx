a6c01e380635b0bc35c6e0b761c694a382581f86
williampariwat <william.pariwat@gmail.com>
2021-02-22 19:04:34 +0700

    Adding firebase

--- src/core/config.tsx
+++ src/core/config.tsx
@@ -1,11 +1,10 @@
 // Replace with your own firebase config!
 export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBDsdN3XWM5D2-c_ti5j44GrhMnBHNmSF0",
-  authDomain: "react-native-market-2861b.firebaseapp.com",
-  databaseURL: "https://react-native-market-2861b.firebaseio.com",
-  projectId: "react-native-market-2861b",
-  storageBucket: "react-native-market-2861b.appspot.com",
-  messagingSenderId: "878215484396",
-  appId: "1:878215484396:web:c36074bccb0b5693b0a6c9",
-  measurementId: "G-317PLP59P2"
+  apiKey: "AIzaSyA5vKPTILWiRPLMdgX2fARk7kFSeJHa4T8",
+  authDomain: "reactive-native-8613e.firebaseapp.com",
+  projectId: "reactive-native-8613e",
+  storageBucket: "reactive-native-8613e.appspot.com",
+  messagingSenderId: "1025080620380",
+  appId: "1:1025080620380:web:a79e8754adcfe7038c3d99",
+  measurementId: "G-75EM1FREG1"
 };
